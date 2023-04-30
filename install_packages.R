e9ed0082af818df88990fa9b172a697e8ac604cb
martibonomi <martina.bonomi2@studio.unibo.it>
2023-04-30 18:56:02 +0200

    Create install_packages.R
    Creating file for installing all required packages for running functions and tests

---
+++ install_packages.R
@@ -0,0 +1,15 @@
+# -------------------------------------------------------------------------------------
+# Author: Martina Bonomi
+# Date: May 2023
+# Rscript : package requirements
+#
+# Name of the project: Visualization and analysis of "FIonA"'s proton treatment plans
+# Aim of the project: to process and visualize proton treatment plans
+#     outcome from "FIonA" treatment planning system
+# --------------------------------------------------------------------------------------
+
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("covr")
+install.packages("testthat")
