175b114f624edac691b975560ffb1934bc9890c4
martibonomi <martina.bonomi2@studio.unibo.it>
2023-05-08 09:40:25 +0200

    Update plans_analysis.R
    Modified script for adapting functions to new changes and commented workflow explaining what to do

--- plans_analysis.R
+++ plans_analysis.R
@@ -8,38 +8,77 @@
 # upload required functions
 source("functions.R")
 
-renamed.structures <- c("CTV", "PTV", "Lungs", "Esophagus", "Heart", "Medulla")
+# ------------------------------ Plan's DVHs Visualization -----------------------------
+
+# defining new names for renaming structures of our plan
+new.names <- c("CTV", "PTV", "Lungs", "Esophagus", "Heart", "Medulla")
+# load plan with new names
+my_plan <- readDVHs(dvhs.csv = "analysis_data/dvhs.csv", renamed.structures = new.names)
+
+# plot plan with title for my plan
+plotDVHs(plan = my_plan, title = TRUE, plan.name = "My Plan")
+
+# defining the structures to keep when filtering our plan
 structures.to.keep <- c("CTV", "PTV")
-
-# Plan's DVHs Visualization
-my_plan <- readDVHs(dvhs.csv = "analysis_data/dvhs.csv", rename.structures = TRUE, structures.names = renamed.structures)
-plotDVHs(plan = my_plan, plan.name = "My Plan", title = TRUE)
-
+# filter the original plan to keep only CTV and PTV structures
 filtered_plan <- selectDVHsStructures(plan = my_plan, keep.structures = structures.to.keep)
-plotDVHs(plan = filtered_plan, plan.name = "My Filtered Plan", title = "Filtered DVHs")
 
-# Quality Parameters
+# plot filtered plan with a specified title
+plotDVHs(plan = filtered_plan, title = "Filtered DVHs")
+
+# ---------------------------- Check quality plan parameters ---------------------------
+
+# calculate V95% for CTV of my plan
 v95 <- getVd(plan = my_plan, d = 95, structure = "CTV")
+
+# calculate V107% for PTV of my plan
 v107 <- getVd(plan = my_plan, d = 107, structure = "PTV")
+
+# calculate D98% for CTV of my plan
 d98 <- getDv(plan = my_plan, v = 98, structure = "CTV")
-d20 <- getDv(plan = my_plan, v = 20, structure = "Lungs")
 
-# Plans' DVHs Comparison
-plan1 <- readDVHs(dvhs.csv = "analysis_data/dvhs.csv", rename.structures = TRUE, structures.names = renamed.structures)
-plan2 <- readDVHs(dvhs.csv = "analysis_data/dvhs_comparison.csv", rename.structures = TRUE, structures.names = renamed.structures)
-plans <- list("Plan 1" = plan1, "Plan 2" = plan2)
-plotComparePlansDVHs(plans = plans, title = TRUE)
+# calculate D2% for CTV of my plan
+d2 <- getDv(plan = my_plan, v = 2, structure = "Lungs")
 
-# Plan's Robustness Assessment
-my_robustness <- readRobustness(robustness.csv = "analysis_data/robustness_dvhs.csv", rename.structures = TRUE, structures.names = renamed.structures)
-plotRobustness(robustness = my_robustness, robustness.name = "My Plan", title = TRUE)
+# ---------------------------- Plan's Robustness Assessment ----------------------------
 
+# defining new names for renaming structures of our plan
+new.names <- c("CTV", "PTV", "Lungs", "Esophagus", "Heart", "Medulla")
+# load robustness dataframe output from FIonA renaming structures
+my_robustness <- readRobustness(robustness.csv = "analysis_data/robustness_dvhs.csv", renamed.structures = new.names)
+
+# plot robustness curves for my structures with no title
+plotRobustness(robustness = my_robustness, title = FALSE)
+
+# selecting structures to keep for robustness
+structures.to.keep <- c("CTV", "Esophagus", "PTV")
+# filtering robustness curves to keep only the ones for selected structures
 filtered.robustness <- selectRobustnessStructures(robustness = my_robustness, keep.structures = structures.to.keep)
-plotRobustness(robustness = filtered.robustness, robustness.name = "My Robustness", title = FALSE)
 
+# plotting robustness curves for filtered structures
+plotRobustness(robustness = filtered.robustness, title = "Filtered Robustness")
+
+# plotting worst and best case scenario for robustness curves, together with nominal curves
 plotRobustnessSpread(robustness = my_robustness, robustness.name = "My Plan", title = TRUE)
 
+# calulating robustness value at V95% for the worst case scenario
 rob_d95 <- getStructureRobustness(robustness = my_robustness, dose = 95, structure = "CTV")
 
-# Plan's Energy Layers
+# ------------------------------------- Plans Energies --------------------------------
+
+# get plan's energy layers (total energies and energies per field)
 energies <- getEnergies("analysis_data/energies.csv")
+
+# -------------------------------- Plans' DVHs Comparison ------------------------------
+
+# defining new names for renaming structures of our plans
+new.names <- c("CTV", "PTV", "Lungs", "Esophagus", "Heart", "Medulla")
+# load two different plans
+plan1 <- readDVHs(dvhs.csv = "analysis_data/dvhs.csv", renamed.structures = new.names)
+plan2 <- readDVHs(dvhs.csv = "analysis_data/dvhs_comparison.csv", renamed.structures = new.names)
+
+# create a list with the two plans to set it as input of the plot function
+plans <- list("Plan 1" = plan1, "Plan 2" = plan2)
+
+# plot the DVHs comparison between the two plans
+plotComparePlansDVHs(plans = plans, title = TRUE)
